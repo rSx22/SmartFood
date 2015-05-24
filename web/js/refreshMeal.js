7f56cfae74ef037ed4b6da1fc5bcfebe3a9b2aff
Benjamin Robert <benjamin.robert@supinternet.fr>
2015-05-24 16:12:50 +0200

    first commit

---
+++ web/js/refreshMeal.js
@@ -0,0 +1,16 @@
+
+
+$(function() {
+    $('.proposition-btn').click(function() {
+        $('.plat1').css('background-image', 'url(./images/asset/carbonara.png)');
+        $('.plat1desc').text('Carbonara');
+    });
+});
+
+$(function() {
+    $('.proposition-btn').click(function() {
+        $('.plat2').css('background-image', 'url(./images/asset/tomates.png)');
+        $('.plat2desc').text('Salade de tomates');
+
+    });
+});
