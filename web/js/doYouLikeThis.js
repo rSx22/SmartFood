193e227f675523a46c28865c42bb2d59b6b6f0c7
Benjamin Robert <benjamin.robert@supinternet.fr>
2015-05-29 01:35:36 +0200

    click color

--- web/js/doYouLikeThis.js
+++ web/js/doYouLikeThis.js
@@ -1,17 +1,48 @@
-$(function() {
-    $('.rate-item').click(function() {
-        $.ajax({
-            url : '?p=addAppreciation',
-            type : 'POST',
-            data : {appreciation: $(this).attr('data-like')},
-            dataType : 'json',
-            success:function(data){
-                console.log(data);
-            },
-            error : function(data) {
-                console.log(data);
+//$(function() {
+//    $('.rate-item').click(function() {
+//        $.ajax({
+//            url : '?p=addAppreciation',
+//            type : 'POST',
+//            data : {appreciation: $(this).attr('data-like')},
+//            dataType : 'json',
+//            success:function(data){
+//                console.log(data);
+//            },
+//            error : function(data) {
+//                console.log(data);
+//
+//            }
+//        });
+//    });
+//});
 
-            }
-        });
+$(function() {
+    $('.note1').click(function() {
+        $('.note1').css('color', 'white');
+    });
+});
+
+
+$(function() {
+    $('.note2').click(function() {
+        $('.note2').css('color', 'white');
+    });
+});
+
+$(function() {
+    $('.note3').click(function() {
+        $('.note3').css('color', 'white');
+    });
+});
+
+$(function() {
+    $('.note4').click(function() {
+        $('.note4').css('color', 'white');
+    });
+});
+
+$(function() {
+    $('.note5').click(function() {
+        $('.note5').css('color', 'white');
     });
 });
